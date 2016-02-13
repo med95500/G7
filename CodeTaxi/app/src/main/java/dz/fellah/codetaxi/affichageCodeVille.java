d5994c67496998b063154c83192764a4a2c8577e
med95500 <med95500@hotmail.com>
2016-02-13 16:24:56 +0100

    création du projet => activité vide

---
+++ CodeTaxi/app/src/main/java/dz/fellah/codetaxi/affichageCodeVille.java
@@ -0,0 +1,13 @@
+package dz.fellah.codetaxi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class affichageCodeVille extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_affichage_code_ville);
+    }
+}
